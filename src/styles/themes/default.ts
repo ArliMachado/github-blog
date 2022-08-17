4f20fd1d7cfe2928be935ea5add3664ed1243386
Arli Machado <arli.machado@gmail.com>
2022-08-16 21:45:56 -0300

    Configurando eslint e editorConfig

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,5 +1,4 @@
 export const defaultTheme = {
-
   blue: '#3294F8',
 
   'base-title': '#E7EDF4',
@@ -8,9 +7,8 @@ export const defaultTheme = {
   'base-span': '#7B96B2',
   'base-label': '#3A536B',
   'base-border': '#1C2F41',
-  'base-post' :'#112131',
+  'base-post': '#112131',
   'base-profile': '#0B1B2B',
   'base-background': '#071422',
-  'base-input': '#040F1A'
-
+  'base-input': '#040F1A',
 } as const
